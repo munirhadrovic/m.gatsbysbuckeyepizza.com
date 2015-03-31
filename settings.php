1f2fffc34d774d18f73e771a7b63ce1450c5f5e1
anes89cok <anes89cok@gmail.com>
2015-03-31 14:51:46 +0200

    First commit

---
+++ settings.php
@@ -0,0 +1,10 @@
+<?php
+	$adresa="4450 Bonita Beach Road"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad="Naples";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica="FL 34134"; // AKo ne znate skracenicu ostavite praznu
+	$telephone=2396765011;// Broj koji se poziva na dugme CALL US
+	$theme="b";//Thema koja se aplicira na sve
+	$transitionefect="fade"; //kucate tranziciju koju zelite na stranama . Vrste tranzicija : "fade", "flip", "pop", "slide", "slidedown", "slideup"
+    $transitionefect1="flip"; //kucate tranziciju koju zelite na stranama . Vrste tranzicija : "fade", "flip", "pop", "slide", "slidedown", "slideup"
+    $transitionefect2="pop"; //kucate tranziciju koju zelite na stranama . Vrste tranzicija : "fade", "flip", "pop", "slide", "slidedown", "slideup"
+?>
